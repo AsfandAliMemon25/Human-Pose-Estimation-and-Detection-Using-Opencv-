cc762b27d6e2c86546421a166ac8164c67bf4f51
AsfandAliMemon25 <154571318+AsfandAliMemon25@users.noreply.github.com>
2023-12-22 18:14:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+import cv2
+import mediapipe as mp
+import cv2 as cv
+import matplotlib.pyplot
+net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
+inWidth = 368
+inHeight = 368
+thr = 0.2
+BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+               "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+               "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+
+POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+               ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+               ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+               ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
+               ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+img = cv.imread("Ali.jpg")
+#cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#import matplotlib.pyplot as plt
+#plt.imshow(img)
+def pose_estimation(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+    out = net.forward()
+    out = out[:, :19, :, :]  # MobileNet output [1, 57, -1, -1], we only need the first 19 elements
+
+    assert (len(BODY_PARTS) == out.shape[1])
+
+    points = []
+    for i in range(len(BODY_PARTS)):
+        # Slice heatmap of corresponging body's part.
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert (partFrom in BODY_PARTS)
+        assert (partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0),  3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
+    t, _ = net.getPerfProfile()
+
+    return frame
+
+vid = cv2.VideoCapture(0)
+
+while (True):
+
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+    estimated_image = pose_estimation(frame)
+    # Display the resulting frame
+    cv2.imshow('frame', estimated_image)
+
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
+"""
+cv2.imshow('image',estimated_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
+
+"""
+
